ae7817e717100d1453da9ae2b9ab6b9700cedf3e
johnluo <johnluo@umich.edu>
2019-09-07 15:57:49 -0400

    Um

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+from random import randint
+a = randint(1,100)
+b = randint(1,100)
+
+result = a + b
+
+print ("John")
+print (a)
+print (b)
+print ("Sum = " + str(result))
+print ("Average = " + str(result/2.0))
